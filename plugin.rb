a6bbb9f9a4a8fcf0eb42062c0dc90f0e0bd7c96c
Angus McLeod <angus@mcleod.org.au>
2017-12-01 12:01:53 +0800

    Rm stylesheet

--- plugin.rb
+++ plugin.rb
@@ -3,8 +3,6 @@
 # version: 0.1
 # authors: angus
 
-register_asset 'stylesheets/layouts-topic-list.scss'
-
 DiscourseEvent.on(:layouts_ready) do
   DiscourseLayouts::WidgetHelper.add_widget('topic-list', position: 'right', order: 'start')
 end
